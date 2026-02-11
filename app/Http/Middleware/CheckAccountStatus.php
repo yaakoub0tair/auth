8a0d1fd57403b950e8efd0dde58e440c1e53b33c
Yaakoub tair <yaakoubt05@gmail.com>
2026-02-11 03:53:17 +0100

    Créer le middleware CheckAccountStatus pour bloquer les comptes désactivés

---
+++ app/Http/Middleware/CheckAccountStatus.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+
+class CheckAccountStatus
+{
+    public function handle(Request $request, Closure $next)
+    {
+        return $next($request);
+    }
+}
